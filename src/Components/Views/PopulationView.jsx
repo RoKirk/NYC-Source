b083666fb732757d99757fc58ca155cb6a3158c2
Ro <hohenkirkrhol@gmail.com>
2020-04-24 10:07:15 -0400

    Fixed background.

--- src/Components/Views/PopulationView.jsx
+++ src/Components/Views/PopulationView.jsx
@@ -1,53 +1,59 @@
-import React from 'react';
+import React, { Component } from 'react';
 import NavBar from '../Shared/NavBar';
 import Table from 'react-bootstrap/Table';
 import { Link } from 'react-router-dom'
 
 
-export const PopulationView = () => {
-    return (
+class PopulationView extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  };
+    }
+    render() {
+        return (
 
-        <div className="PopulationView">
+            <div className="PopulationView">
 
-            <h1 className="heading">Population</h1>
+                <h1 className="heading">Population</h1>
 
-            <aside>
-                <NavBar />
-            </aside>
+                <aside>
+                    <NavBar />
+                </aside>
 
-            <Table className="table" responsive="sm" striped bordered hover variant="dark">
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>1</td>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                    </tr>
-                    <tr>
-                        <td>2</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                    </tr>
-                    <tr>
-                        <td>3</td>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        {/* <td colSpan="2">Larry the Bird</td> */}
-                    </tr>
-                </tbody>
-            </Table>
+                <Table className="table" responsive="sm" striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                        </tr>
+                        <tr>
+                            <td>2</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                        </tr>
+                        <tr>
+                            <td>3</td>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            {/* <td colSpan="2">Larry the Bird</td> */}
+                        </tr>
+                    </tbody>
+                </Table>
 
-            <Link to="/">Back to Landing</Link>
+                <Link to="/">Back to Landing</Link>
 
-           
-        </div>
-    )
+            </div>
+
+        );
+    }
 }
 
 export default PopulationView;
